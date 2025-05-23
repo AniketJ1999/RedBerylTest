d66af5060d694c20ec72ad2fda4e594f77fadfe4
AniketJ1999 <166418933+AniketJ1999@users.noreply.github.com>
2025-05-24 01:48:50 +0530

    Update OrdersList.jsx
    replaced icons

--- src/components/OrdersList.jsx
+++ src/components/OrdersList.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
-import { ChevronDownIcon, ArrowUpDownIcon } from 'lucide-react';
+import { ChevronDownIcon } from 'lucide-react';
+import { BiSortDown } from "react-icons/bi";
+import { MdRefresh } from "react-icons/md";
+
 export const OrdersList = ({
   orders,
   selectedOrder,
@@ -23,7 +26,7 @@ export const OrdersList = ({
       <div className="border-b-2 px-4 py-3 flex items-center justify-between">
         <div className="flex items-center">
           <span className="font-medium">Orders</span>
-          <span className="ml-2 bg-blue-100 text-blue-800 text-xs px-2 py-0.5 rounded-full">
+          <span className="ml-2  bg-blue-100 text-blue-800 text-xs font-bold px-2 py-1.5 rounded-2xl">
             {orders.length}
           </span>
         </div>
@@ -32,10 +35,10 @@ export const OrdersList = ({
             Order Number &nbsp; <ChevronDownIcon size={16} className="ml-1" />
           </button>
           <button className="ml-2 p-2 text-gray-500 hover:text-gray-700 bg-white rounded border border-gray-300">
-            <ArrowUpDownIcon size={16} />
+            <BiSortDown size={20} />
           </button>
           <button className="ml-2 p-2 text-gray-500 hover:text-gray-700 bg-white rounded border border-gray-300">
-            <ArrowUpDownIcon size={16}  />
+            <MdRefresh size={20}  />
           </button>
         </div>
       </div>
@@ -67,9 +70,9 @@ const getStatusBadgeColor = status => {
   switch (status.toLowerCase()) {
     case 'draft':
       return 'bg-gray-100 text-gray-800';
-    case 'in progress':
+    case 'order in progress':
       return 'bg-green-100 text-green-800';
-    case 'created':
+    case 'order created':
       return 'bg-blue-100 text-blue-800';
     default:
       return 'bg-gray-100 text-gray-800';

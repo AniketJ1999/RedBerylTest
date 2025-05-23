848a7e356f2c6a6e65be507504c53ccf2a94dae8
AniketJ1999 <166418933+AniketJ1999@users.noreply.github.com>
2025-05-24 01:47:30 +0530

    Update Header.jsx
    added icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import vstate from '../assets/vstate.png'
+import { FaRegBuilding } from "react-icons/fa";
+
 export const Header = () => {
   return <div className="header">
     <div > <img src={vstate} className="vstate"  /></div>
         <div className="user">
             <span className="text-blue-600 mr-2">India Corp</span>
-          <span className="text-blue-600">↑</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <span className="text-blue-600"><FaRegBuilding /></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
        
        
-          <span className="mr-2">Welcome, Mayuri Chavan</span>
+          <span className="text-blue-600 mr-2">Welcome, Mayuri Chavan</span>
           <div className="w-8 h-8 rounded-full bg-purple-600 text-white flex items-center justify-center text-sm">
             MC
           </div>
